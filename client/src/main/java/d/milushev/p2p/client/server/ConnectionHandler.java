9d859bc51f10baa15cbe9185e1f35e4523ab1b2e
Dimitar Milushev <d.milushev@seeburger.com>
2026-08-12 18:23:17 +0300

    extract connection logic to ConnectionHandler

---
+++ client/src/main/java/d/milushev/p2p/client/server/ConnectionHandler.java
@@ -0,0 +1,122 @@
+package main.java.d.milushev.p2p.client.server;
+
+
+import d.milushev.p2p.network_utils.SocketUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+public class ConnectionHandler
+{
+    private static final Logger LOG = LogManager.getLogger(ConnectionHandler.class);
+    private static final int DEFAULT_BUFFER_SIZE = 1024;
+
+    private final Queue<String> commandsQueue;
+    private final AtomicBoolean awaitingResponse;
+
+    private final ByteBuffer writeBuffer;
+    private final ByteBuffer readBuffer;
+
+
+    public ConnectionHandler()
+    {
+        commandsQueue = new LinkedList<>();
+        awaitingResponse = new AtomicBoolean(false);
+
+        writeBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
+        readBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
+    }
+
+
+    public void handleWrite(SelectionKey key)
+                    throws Exception
+    {
+        if (commandsQueue.isEmpty())
+        {
+            return;
+        }
+
+        LOG.info("Handling WRITE for channel: {}", key.channel());
+        writeBuffer.clear();
+
+        final String message = commandsQueue.poll();
+        if (message == null || message.isBlank())
+        {
+            LOG.warn("Empty command. Ignoring...");
+            return;
+        }
+
+        if (key.channel() instanceof SocketChannel server)
+        {
+            final Socket socket = server.socket();
+
+            LOG.info("Handling WRITE for [{}]", socket.getRemoteSocketAddress());
+
+            SocketUtils.writeToChannel(server, writeBuffer, message);
+
+            key.interestOps(SelectionKey.OP_READ);
+            awaitingResponse.set(true);
+
+            LOG.debug("Successfully handled WRITE for [{}]: {}", socket.getRemoteSocketAddress(), message);
+        }
+    }
+
+
+    public void handleRead(SelectionKey key)
+                    throws Exception
+    {
+        LOG.info("Handling READ for channel: {}", key.channel());
+
+        if (key.channel() instanceof SocketChannel clientChannel)
+        {
+            final String message = SocketUtils.readFromChannel(clientChannel, readBuffer);
+            LOG.info("Received: {}", message);
+
+            awaitingResponse.set(false);
+            key.interestOps(SelectionKey.OP_WRITE);
+
+            return;
+        }
+
+        throw new Exception("Invalid channel was opened for READ operation");
+    }
+
+
+    public void handleConnect(SelectionKey key)
+                    throws Exception
+    {
+        if (key.channel() instanceof SocketChannel clientChannel)
+        {
+            while (!clientChannel.finishConnect())
+            {
+                LOG.info("Connecting...");
+            }
+
+            LOG.info("Connected to server: {}", clientChannel.getRemoteAddress());
+            key.interestOps(SelectionKey.OP_WRITE);
+            return;
+        }
+
+        throw new Exception("Invalid channel was opened for CONNECT operation");
+    }
+
+
+    public void enqueueMessage(String message)
+                    throws Exception
+    {
+        if (!commandsQueue.add(message))
+        {
+            throw new Exception("Failed to enqueue message: " + message);
+        }
+
+        LOG.info("Successfully enqueued message: {}", message);
+    }
+}
--- client/src/main/java/d/milushev/p2p/client/server/ServerCommunicator.java
+++ client/src/main/java/d/milushev/p2p/client/server/ServerCommunicator.java
@@ -1,27 +1,14 @@
 package main.java.d.milushev.p2p.client.server;
 
 
-import d.milushev.p2p.network_utils.SocketUtils;
-import d.milushev.p2p.network_utils.models.Request;
-import d.milushev.p2p.network_utils.models.Response;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Consumer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,49 +19,32 @@ import org.apache.logging.log4j.Logger;
  */
 public class ServerCommunicator
 {
-    private final static Logger LOG = LogManager.getLogger(ServerCommunicator.class.getName());
-
-    private final static int DEFAULT_BUFFER_SIZE = 1024;
+    private static final Logger LOG = LogManager.getLogger(ServerCommunicator.class.getName());
 
     private final InetSocketAddress targetAddress;
     private final AtomicBoolean running;
-    private final AtomicBoolean awaitingResponse;
-    private final Queue<String> commandsQueue;
 
     private final ExecutorService executor;
+    private final ConnectionHandler connectionHandler;
 
 
     public ServerCommunicator(String host, int port)
     {
         this.targetAddress = new InetSocketAddress(host, port);
         this.running = new AtomicBoolean(false);
-        this.commandsQueue = new LinkedList<>();
         this.executor = Executors.newSingleThreadExecutor();
-        awaitingResponse = new AtomicBoolean(false);
+        this.connectionHandler = new ConnectionHandler();
     }
 
 
     public void start()
                     throws IOException
     {
-        executor.submit(() -> {
-            try
-            {
-                this.listen();
-            } catch (IOException e)
-            {
-                LOG.warn("Error starting server communicator", e);
-            }
-            finally
-            {
-                executor.shutdown();
-            }
-        });
+        executor.submit(this::openConnection);
     }
 
 
-    private void listen()
-                    throws IOException
+    private void openConnection()
     {
         try (Selector selector = Selector.open();
              SocketChannel clientChannel = SocketChannel.open())
@@ -83,49 +53,18 @@ public class ServerCommunicator
             clientChannel.connect(targetAddress);
             clientChannel.register(selector, SelectionKey.OP_CONNECT);
 
-            final ByteBuffer writeBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
-            final ByteBuffer readBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
-
             running.set(true);
             LOG.info("Client started. Type messages to send:");
 
-            while (running.get())
-            {
-                try
-                {
-                    if (selector.select(1000) == 0)
-                    {
-                        continue;
-                    }
-                }
-                catch (IOException e)
-                {
-                    throw new RuntimeException(e);
-                }
-
-                for (var key : selector.selectedKeys())
-                {
-                    if (key.isWritable())
-                    {
-                        handleWrite(key, writeBuffer);
-                    }
-                    else if (key.isReadable())
-                    {
-                       handleRead(key, clientChannel, readBuffer);
-                    }
-                    else if (key.isConnectable())
-                    {
-                        while (!clientChannel.finishConnect())
-                        {
-                            System.out.println("Connecting...");
-                        }
-                        System.out.println("Connected");
-                        key.interestOps(SelectionKey.OP_WRITE);
-                    }
-                }
-
-                selector.selectedKeys().clear();
-            }
+            listenOnChannel(selector);
+        }
+        catch (IOException e)
+        {
+            LOG.warn("Error starting server communicator", e);
+        }
+        catch (Exception e)
+        {
+            LOG.error("Error in server communicator", e);
         }
         finally
 
@@ -137,51 +76,57 @@ public class ServerCommunicator
     }
 
 
-    private void handleRead(SelectionKey key, SocketChannel clientChannel, ByteBuffer readBuffer)
-                    throws IOException
+    private void listenOnChannel(Selector selector)
+                    throws Exception
     {
-        final String message = SocketUtils.readFromChannel(clientChannel, readBuffer);
-        LOG.info("Received: {}", message);
-        awaitingResponse.set(false);
-        key.interestOps(SelectionKey.OP_WRITE);
-    }
-
-
-    private void handleWrite(SelectionKey key, ByteBuffer writeBuffer)
-                    throws IOException
-    {
-        if (commandsQueue.isEmpty())
+        while (running.get())
         {
-            return;
-        }
+            try
+            {
+                if (selector.select(1000) == 0)
+                {
+                    continue;
+                }
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
 
-        writeBuffer.clear();
+            for (var key : selector.selectedKeys())
+            {
+                if (key.isWritable())
+                {
+                    connectionHandler.handleWrite(key);
+                }
+                else if (key.isReadable())
+                {
+                    connectionHandler.handleRead(key);
+                }
+                else if (key.isConnectable())
+                {
+                    connectionHandler.handleConnect(key);
+                }
+                else
+                {
+                    LOG.warn("Unknown key state: {}", key);
+                }
+            }
 
-        final String message = commandsQueue.poll();
-        if (message == null || message.isBlank())
-        {
-            LOG.warn("Empty command. Ignoring...");
-            return;
-        }
-
-        if (key.channel() instanceof SocketChannel server)
-        {
-            final Socket socket = server.socket();
-
-            LOG.info("Handling WRITE for [{}]", socket.getRemoteSocketAddress());
-
-            SocketUtils.writeToChannel(server, writeBuffer, message);
-
-            key.interestOps(SelectionKey.OP_READ);
-            awaitingResponse.set(true);
-
-            LOG.debug("Successfully handled WRITE for [{}]: {}", socket.getRemoteSocketAddress(), message);
+            selector.selectedKeys().clear();
         }
     }
 
 
     public void send(String message)
     {
-        commandsQueue.add(message);
+        try
+        {
+            connectionHandler.enqueueMessage(message);
+        }
+        catch (Exception e)
+        {
+            LOG.error("Error when sending message: {}", message, e);
+        }
     }
 }
