7bc5c153e09c7a4ff083dc6f3089a8a27990b9fe
D_M <DimitarMilushev5@gmail.com>
2026-08-25 15:12:10 +0300

    Env Utils (#7)
    * init env project
    
    * Add env project to root gradle
    
    ---------
    
    Co-authored-by: Dimitar Milushev <d.milushev@seeburger.com>

---
+++ env-utils/src/main/java/d/milushev/p2p/env_utils/data/EnvData.java
@@ -0,0 +1,51 @@
+package d.milushev.p2p.envutil.data;
+
+
+public abstract class EnvData<T>
+{
+    private final String envName;
+    private final T defaultValue;
+
+
+    protected EnvData(String envName, T defaultValue)
+    {
+        this.envName = envName;
+        this.defaultValue = defaultValue;
+    }
+
+
+    public T getDefaultValue()
+    {
+        return defaultValue;
+    }
+
+
+    public T getValue()
+    {
+        final String stringValue = System.getenv(envName);
+        if (stringValue == null)
+        {
+            return null;
+        }
+
+        return parseValue(stringValue);
+    }
+
+
+    public T getOrDefault()
+    {
+        final T value = getValue();
+        if (value == null)
+        {
+            return getDefaultValue();
+        }
+
+        return value;
+    }
+
+
+    /**
+     * Parses the {@link String} value into {@link T}.
+     */
+    protected abstract T parseValue(String value);
+}
